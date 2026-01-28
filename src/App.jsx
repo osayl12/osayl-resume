e03446c5cb54457358c5aee65e230e9f1cb8cd51
osayl12 <hamedosayl@gmail.com>
2026-01-28 09:40:50 +0200

    feat: compose page layout in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import "./styles.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
-function App() {
-
+export default function App() {
   return (
-    <>
-
-    </>
-  )
+    <div className="container">
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Footer />
+    </div>
+  );
 }
-
-export default App
